da446e580ac97fa3c21e8c986f6653873f7d6d03
KSanthanesh <santhanesh_2004@yahoo.com>
2021-07-06 15:00:28 +0000

    Add surplus data

--- run.py
+++ run.py
@@ -76,7 +76,6 @@ def calculate_surplus_data(sales_row):
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
-    print(surplus_data)
 
     return surplus_data
 
